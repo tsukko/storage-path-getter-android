41eaa5c89ce71427f448f0fb9dc57ecff4c8a5b6
tsukko <tsukko@gmail.com>
2021-02-11 01:47:52 +0900

    update library version

--- app/src/main/java/jp/co/integrityworks/storagepathgetter/ui/MainActivity.kt
+++ app/src/main/java/jp/co/integrityworks/storagepathgetter/ui/MainActivity.kt
@@ -4,19 +4,17 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.initialization.InitializationStatus
 import jp.co.integrityworks.storagepathgetter.BuildConfig
+import jp.co.integrityworks.storagepathgetter.Logger
 import jp.co.integrityworks.storagepathgetter.R
 import jp.co.integrityworks.storagepathgetter.Utils
-import kotlinx.android.synthetic.main.activity_main.*
-import java.io.ByteArrayOutputStream
-import java.io.File
-import java.util.zip.InflaterInputStream
+import jp.co.integrityworks.storagepathgetter.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,30 +22,35 @@ class MainActivity : AppCompatActivity() {
         private val TAG = MainActivity::class.java.simpleName
     }
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Logger.debug(TAG, "onCreate")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         supportActionBar?.title =
             if (BuildConfig.DEBUG) getString(R.string.app_name) + " (deb)" else getString(R.string.app_name)
 
         //クリップボードのサービスのインスタンスを取得する
         val mManager: ClipboardManager =
             applicationContext.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        copyInternalButton.setOnClickListener {
-            val myClip: ClipData = ClipData.newPlainText("label", internalPathEditText.text)
+        binding.copyInternalButton.setOnClickListener {
+            val myClip: ClipData = ClipData.newPlainText("label", binding.internalPathEditText.text)
             mManager.setPrimaryClip(myClip)
             Toast.makeText(
                 applicationContext,
-                "内部ストレージのパスをコピーしました。\n" + internalPathEditText.text,
+                "内部ストレージのパスをコピーしました。\n" + binding.internalPathEditText.text,
                 Toast.LENGTH_LONG
             ).show()
         }
-        copyExternalButton.setOnClickListener {
-            val myClip: ClipData = ClipData.newPlainText("label", internalPathEditText.text)
+        binding.copyExternalButton.setOnClickListener {
+            val myClip: ClipData = ClipData.newPlainText("label", binding.externalPathEditText.text)
             mManager.setPrimaryClip(myClip)
             Toast.makeText(
                 applicationContext,
-                "外部ストレージのパスをコピーしました。\n" + externalPathEditText.text,
+                "外部ストレージのパスをコピーしました。\n" + binding.externalPathEditText.text,
                 Toast.LENGTH_LONG
             ).show()
         }
@@ -68,59 +71,27 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        }
 
-        getPathButton.setOnClickListener { init() }
-        clearButton.setOnClickListener {
-            internalPathEditText.text.clear()
-            externalPathEditText.text.clear()
-            sizeTextView.text = ""
+        binding.getPathButton.setOnClickListener { init() }
+        binding.clearButton.setOnClickListener {
+            binding.internalPathEditText.text?.clear()
+            binding.externalPathEditText.text?.clear()
+            binding.sizeTextView.text = ""
         }
 
-        MobileAds.initialize(this, BuildConfig.admob_app_id)
-        val adRequest: AdRequest = AdRequest.Builder().build()
-        adView.loadAd(adRequest)
-    }
-
-    private fun aaaaaaa() {
-        val b = String(Base64.decode("c2V0Q29tcG9uZW50RW5hYmxlZFNldHRpbmc=", 0))
-        Log.d("debugggggg", b)
-
-        try {
-            val bbb = StringBuilder()
-            bbb.append(filesDir.absolutePath)
-            Log.d("debugggggg1", bbb.toString())
-            bbb.append(File.separator)
-            Log.d("debugggggg2", bbb.toString())
-            bbb.append("DEx".toLowerCase())
-            Log.d("debugggggg3", bbb.toString())
-            val file = File(bbb.toString())
-            if (file.exists())
-                file.delete()
-            val byteArrayOutputStream = ByteArrayOutputStream()
-            val assss = resources.assets
-            val ccc = StringBuilder();
-            ccc.append("fas/".substring(1).toLowerCase())
-            Log.d("debugggggg4", ccc.toString())
-            ccc.append(assss.list("as")!![0])
-            Log.d("debugggggg5", ccc.toString())
-            var inputStream = assss.open(ccc.toString())
-            inputStream.skip(4L)
-            inputStream = InflaterInputStream(inputStream)
-            val arrayOfByte = ByteArray(2048)
-            while (true) {
-                val i = inputStream.read(arrayOfByte)
-                if (i == -1) {
-                    inputStream.close()
-//                    obbbbbj_a(file, byteArrayOutputStream)
-//                    obbbbbj_a(file)
-                    return
-                }
-                byteArrayOutputStream.write(arrayOfByte, 0, i)
+//        MobileAds.initialize(this, BuildConfig.admob_app_id)
+        MobileAds.initialize(this) { initializationStatus: InitializationStatus ->
+            /* get the adapter status */
+            val map =
+                initializationStatus.adapterStatusMap
+            for ((key, adapterStatus) in map) {
+                val state = adapterStatus.initializationState
+                Logger.debug(TAG,
+                    "key = " + key + ", state = " + state.name + ", desc = " + adapterStatus.description
+                )
             }
-        } catch (throwable: Throwable) {
-            throwable.printStackTrace()
-            return
         }
-
+        val adRequest: AdRequest = AdRequest.Builder().build()
+        binding.adView.loadAd(adRequest)
     }
 
     override fun onResume() {
@@ -129,12 +100,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
+        Logger.debug(TAG, "init")
         val util = Utils(applicationContext)
         val internalPath = util.getPath(false)
         val externalPath = util.getPath(true)
-        internalPathEditText.setText(internalPath)
-        externalPathEditText.setText(externalPath)
-
-        sizeTextView.text = util.getMemoryInformation(internalPath, externalPath)
+        binding.internalPathEditText.setText(internalPath)
+        binding.externalPathEditText.setText(externalPath)
+        binding.sizeTextView.text = util.getMemoryInformation(internalPath, externalPath)
     }
 }
