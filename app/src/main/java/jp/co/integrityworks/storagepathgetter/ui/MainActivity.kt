21ed7d3fc7667275202fa5d619ea6f86a02fe221
tsukko <tsukko@gmail.com>
2023-08-31 15:40:43 +0900

    update targetSdkVersion

--- app/src/main/java/jp/co/integrityworks/storagepathgetter/ui/MainActivity.kt
+++ app/src/main/java/jp/co/integrityworks/storagepathgetter/ui/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.AdRequest
