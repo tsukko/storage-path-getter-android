ffd7a9e795d70d2a5f93b9587d8d288620398f3c
tsukko <tsukko@gmail.com>
2026-05-27 17:57:11 +0900

    feat: update version code and name to 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "jp.co.integrityworks.storagepathgetter"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 6
-        versionName = "1.3"
+        versionCode = 7
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
